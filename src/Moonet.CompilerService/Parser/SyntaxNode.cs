a2d1ea9aa4b6d10cbdac1e510117461967f5db77
Blealtan Cao <blealtan@gmail.com>
2017-01-27 16:35:01 +0800

    Add SyntaxNode.

---
+++ src/Moonet.CompilerService/Parser/SyntaxNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonet.CompilerService.Parser
+{
+    class SyntaxNode
+    {
+        public int Line { get; }
+
+        public int Colomn { get; }
+
+        public SyntaxNode(int line, int colomn)
+        {
+            Line = line;
+            Colomn = colomn;
+        }
+    }
+}

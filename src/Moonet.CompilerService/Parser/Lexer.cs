92d6e4c98bae8edc3d88af9fc994fea522634aba
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-01-27 10:09:56 +0800

    Unify MatchNumber.

--- src/Moonet.CompilerService/Parser/Lexer.cs
+++ src/Moonet.CompilerService/Parser/Lexer.cs
@@ -391,113 +391,72 @@ namespace Moonet.CompilerService.Parser
                 return r;
             }
 
+            int number(char ch) => ch <= '9' ? ch - '0' : ch - 'a' + 10;
+
             string src = CurrentLine.Substring(_colomn).ToLower();
+            int pos, scale, powerBase;
+            bool allowAlphabet;
             if (src.StartsWith("0x")) // Hexadecimal
             {
-                // Integer part.
-                int pos = 2;
-                int integerPart = 0;
-                while ((src[pos] >= '0' && src[pos] <= '9') || (src[pos] >= 'a' && src[pos] <= 'f'))
-                {
-                    integerPart <<= 4;
-                    integerPart += src[pos] - (src[pos] <= '9' ? '0' : 'a');
-                    ++pos;
-                }
-                // If no float part or exponent, it's a integer.
-                if (src[pos] != '.' && src[pos] != 'p')
-                    return new Token<int>(TokenType.Integer, integerPart);
-
-                // Then for floats.
-                double floatResult = integerPart;
-                // Float part.
-                if (src[pos] == '.')
-                {
-                    ++pos;
-                    int floatLen = 0;
-                    int floatPart = 0;
-                    while ((src[pos] >= '0' && src[pos] <= '9') || (src[pos] >= 'a' && src[pos] <= 'f'))
-                    {
-                        floatPart <<= 4;
-                        floatPart += src[pos] - (src[pos] <= '9' ? '0' : 'a');
-                        ++pos;
-                        ++floatLen;
-                    }
-                    floatResult += floatPart / (double)pow(16, floatLen);
-                }
-                // Binary exponent.
-                if (src[pos] == 'p')
-                {
-                    ++pos;
-                    bool neg = false;
-                    if (src[pos] == '+') ++pos;
-                    else if (src[pos] == '-') { neg = true; ++pos; }
-
-                    // Actual exponent number.
-                    int exp = 0;
-                    while (src[pos] >= '0' && src[pos] <= '9')
-                    {
-                        exp *= 10;
-                        exp += src[pos] - '0';
-                        ++pos;
-                    }
-                    if (neg) floatResult /= pow(2, exp);
-                    else floatResult *= pow(2, exp);
-                }
-                return new Token<double>(TokenType.Float, floatResult);
+                pos = 2;
+                scale = 16;
+                powerBase = 2;
+                allowAlphabet = true;
             }
             else
             {
-                // Integer part.
-                int pos = 0;
-                int integerPart = 0;
+                pos = 0;
+                scale = 10;
+                powerBase = 10;
+                allowAlphabet = false;
+            }
+            // Integer part.
+            int integerPart = 0;
+            while ((src[pos] >= '0' && src[pos] <= '9') || (allowAlphabet && src[pos] >= 'a' && src[pos] <= 'f'))
+                integerPart = integerPart * scale + number(src[pos++]);
+            // If no float part or exponent, it's a integer.
+            if (src[pos] != '.' && src[pos] != 'p')
+            {
+                _colomn += pos;
+                return new Token<int>(TokenType.Integer, integerPart);
+            }
+
+            // Then for floats.
+            double floatResult = integerPart;
+            // Float part.
+            if (src[pos] == '.')
+            {
+                ++pos;
+                int floatLen = 0;
+                int floatPart = 0;
+                while ((src[pos] >= '0' && src[pos] <= '9') || (allowAlphabet && src[pos] >= 'a' && src[pos] <= 'f'))
+                {
+                    floatPart = floatPart * scale + number(src[pos++]);
+                    ++floatLen;
+                }
+                floatResult += floatPart / (double)pow(scale, floatLen);
+            }
+            // Exponent.
+            if (src[pos] == 'p')
+            {
+                ++pos;
+                bool neg = false;
+                if (src[pos] == '+') ++pos;
+                else if (src[pos] == '-') { neg = true; ++pos; }
+
+                // Actual exponent number.
+                int exp = 0;
                 while (src[pos] >= '0' && src[pos] <= '9')
                 {
-                    integerPart *= 10;
-                    integerPart += src[pos] - '0';
+                    exp *= 10;
+                    exp += src[pos] - '0';
                     ++pos;
                 }
-                // If no float part or exponent, it's a integer.
-                if (src[pos] != '.' && src[pos] != 'e')
-                    return new Token<int>(TokenType.Integer, integerPart);
-
-                // Then for floats.
-                double floatResult = integerPart;
-                // Float part.
-                if (src[pos] == '.')
-                {
-                    ++pos;
-                    int floatLen = 0;
-                    int floatPart = 0;
-                    while (src[pos] >= '0' && src[pos] <= '9')
-                    {
-                        floatPart *= 10;
-                        floatPart += src[pos] - '0';
-                        ++pos;
-                        ++floatLen;
-                    }
-                    floatResult += floatPart / pow(10, floatLen);
-                }
-                // Exponent.
-                if (src[pos] == 'p')
-                {
-                    ++pos;
-                    bool neg = false;
-                    if (src[pos] == '+') ++pos;
-                    else if (src[pos] == '-') { neg = true; ++pos; }
-
-                    // Actual exponent number.
-                    int exp = 0;
-                    while (src[pos] >= '0' && src[pos] <= '9')
-                    {
-                        exp *= 10;
-                        exp += src[pos] - '0';
-                        ++pos;
-                    }
-                    if (neg) floatResult /= pow(2, exp);
-                    else floatResult *= pow(2, exp);
-                }
-                return new Token<double>(TokenType.Float, floatResult);
+                if (neg) floatResult /= pow(powerBase, exp);
+                else floatResult *= pow(powerBase, exp);
             }
+            _colomn += pos;
+            return new Token<double>(TokenType.Float, floatResult);
         }
 
         private Token<string> MatchRawString()

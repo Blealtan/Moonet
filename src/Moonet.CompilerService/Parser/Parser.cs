9b6242b58b7e553d360edeb4c422f7ff3cf89608
Blealtan Cao <blealtan@gmail.com>
2017-01-29 22:39:40 +0800

    Using stage of parser finished.

--- src/Moonet.CompilerService/Parser/Parser.cs
+++ src/Moonet.CompilerService/Parser/Parser.cs
@@ -12,33 +12,92 @@ namespace Moonet.CompilerService.Parser
 
         private Token _current;
 
+        private TokenType Type { get => _current.Type; }
+
+        private int IntegerValue { get => (_current as Token<int>).Value; }
+
+        private double FloatValue { get => (_current as Token<double>).Value; }
+
+        private string StringValue { get => (_current as Token<string>).Value; }
+
         private Lexer _lexer;
 
-        private Token Accept(TokenType type)
+        private void Next()
         {
-            if (_current.Type == type)
-            {
-                var res = _current;
-                (_line, _colomn, _current) = _lexer.AnalyzeNextToken();
-                return res;
-            }
-            else return null;
+            (_line, _colomn, _current) = _lexer.AnalyzeNextToken();
         }
 
         public Queue<Error> ErrorQueue { get; }
+
+        private readonly int _maxErrors;
 
         private void AddError(string message)
         {
             ErrorQueue.Enqueue(new Error(_line, _colomn, _lexer.CurrentLine, message));
         }
 
-        public Parser(TextReader src)
+        public Parser(TextReader src, int maxErrors)
         {
             ErrorQueue = new Queue<Error>();
             _lexer = new Lexer(src, ErrorQueue);
+            _maxErrors = maxErrors;
         }
 
-        public FileSyntaxTree Parse()
+        public SyntaxTree Parse()
+        {
+            var tree = new SyntaxTree();
+            // Start prediction
+            Next();
+            if (ParseUsing(tree.Usings) && ParseBody(tree.Body))
+                return tree;
+            else return null;
+        }
+
+        private bool ParseUsing(ICollection<UsingSyntax> usings)
+        {
+            while (Type == TokenType.Using)
+            {
+                if (ErrorQueue.Count >= _maxErrors) return false;
+                int line = _line, colomn = _colomn;
+                Next();
+                switch (Type)
+                {
+                    case TokenType.StringLiteral:
+                        var file = (_current as Token<string>).Value;
+                        Next();
+                        string asVar = null;
+                        if (Type == TokenType.As)
+                        {
+                            Next();
+                            if (Type != TokenType.Name)
+                                AddError("Expected variable name after 'as' in using statement.");
+                            else
+                            {
+                                asVar = StringValue;
+                                Next();
+                            }
+                        }
+                        usings.Add(new UsingFileSyntax(line, colomn, file, asVar));
+                        break;
+                    case TokenType.Namespace:
+                        Next();
+                        if (_current.Type == TokenType.StringLiteral)
+                        {
+                            usings.Add(new UsingNamespaceSyntax(line, colomn, StringValue));
+                            Next();
+                        }
+                        else
+                            AddError("Expected string literal after 'namespace' in using statement.");
+                        break;
+                    default:
+                        AddError("Expected string literal or 'namespace' after 'using' in using statement.");
+                        continue;
+                }
+            }
+            return true;
+        }
+
+        private bool ParseBody(BlockSyntax body)
         {
             throw new NotImplementedException();
         }
---
+++ src/Moonet.CompilerService/Syntax/SyntaxTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Moonet.CompilerService.Syntax
+{
+    public class SyntaxTree
+    {
+        public ICollection<UsingSyntax> Usings { get; } = new List<UsingSyntax>();
+
+        public BlockSyntax Body { get; } = new BlockSyntax(1, 1);
+
+        //!TODO: Class definitions
+
+        public SyntaxTree()
+        {
+        }
+    }
+}

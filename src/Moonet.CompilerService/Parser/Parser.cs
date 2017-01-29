58df4fa84aa1b9bf55a21fe38f095e7f9670c65b
Blealtan Cao <blealtan@gmail.com>
2017-01-29 14:31:10 +0800

    Basic helper functions in parser.

---
+++ src/Moonet.CompilerService/Parser/Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonet.CompilerService.Parser
+{
+    public class Parser
+    {
+        private int _line, _colomn;
+
+        private Token _current;
+
+        private Lexer _lexer;
+
+        private Token Accept(TokenType type)
+        {
+            if (_current.Type == type)
+            {
+                var res = _current;
+                (_line, _colomn, _current) = _lexer.AnalyzeNextToken();
+                return res;
+            }
+            else return null;
+        }
+    }
+}

257c79a930f1bbea3d73924fa5610e11383be9c4
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-02-03 16:05:23 +0800

    Basic prototype for testing Lexer.

---
+++ src/Tests/Moonet.CompilerTest/Utilities/LexerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Moonet.CompilerService;
+using Moonet.CompilerService.Parser;
+using Xunit;
+
+namespace Moonet.CompilerTest.Utilities
+{
+    internal class LexerTest
+    {
+        public void TestTokenStream(TextReader input, IEnumerable<Token> tokens)
+        {
+            var errors = new Queue<Error>();
+            var lexer = new Lexer(input, errors);
+            foreach (var token in tokens)
+                Assert.Equal(token, lexer.AnalyzeNextToken().token);
+            Assert.True(errors.Count == 0);
+        }
+    }
+}
